cb86bef4bebcd9d6b6917d32e9a8f3c36365ad50
Dan Mercer <danielmercer1@gmail.com>
2020-08-22 23:59:24 -0600

    Improve html2md

--- web/html2md.spec.ts
+++ web/html2md.spec.ts
@@ -1,7 +1,45 @@
 import { html2md } from './html2md';
+import { demargin } from '../common/string/string';
 
 describe('html2md', () => {
-  it('should work', () => {
+  it('should work on very basic text', () => {
     expect(html2md('<h1>foo</h1>')).toBe("# foo");
   });
+
+  it('should work on a large, clean HTML snippet', () => {
+    expect(html2md(demargin`
+      <span><p>You can combine <code>async</code> and <code>await</code> with <code>.resolves</code> or <code>.rejects</code>.</p>
+      <pre><code class="hljs css language-js">test(<span class="hljs-string">'the data is peanut butter'</span>, <span class="hljs-keyword">async</span> () =&gt; {
+        <span class="hljs-keyword">await</span> expect(fetchData()).resolves.toBe(<span class="hljs-string">'peanut butter'</span>);
+      });
+
+      test(<span class="hljs-string">'the fetch fails with an error'</span>, <span class="hljs-keyword">async</span> () =&gt; {
+        <span class="hljs-keyword">await</span> expect(fetchData()).rejects.toThrow(<span class="hljs-string">'error'</span>);
+      });
+      </code></pre></span><span>
+      <p>In these cases, <code>async</code> and <code>await</code> are effectively syntactic sugar for the same logic as the promises example uses.</p>
+      <p>None of these forms is particularly superior to the others, and you
+      can mix and match them across a codebase or even in a single file. It
+      just depends on which style you feel makes your tests simpler.</p></span>
+    `)).toBe(demargin`
+      You can combine \`async\` and \`await\` with \`.resolves\` or \`.rejects\`.
+
+      \`\`\`
+      test('the data is peanut butter', async () => {
+        await expect(fetchData()).resolves.toBe('peanut butter');
+      });
+
+      test('the fetch fails with an error', async () => {
+        await expect(fetchData()).rejects.toThrow('error');
+      });
+
+      \`\`\`
+
+      In these cases, \`async\` and \`await\` are effectively syntactic sugar for the same logic as the promises example uses.
+
+      None of these forms is particularly superior to the others, and you
+      can mix and match them across a codebase or even in a single file. It
+      just depends on which style you feel makes your tests simpler.
+    `.trim())
+  })
 });
--- web/html2md.ts
+++ web/html2md.ts
@@ -1,86 +1,105 @@
 export function html2md(html: string): string {
   const parsed = new DOMParser().parseFromString(html, 'text/html');
-  return element2md(parsed.body).trim();
+  return element2md(parsed.body)
+    .trim()
+    .replace(/[ \t]+\n/g, '\n') // trim whitespace at EOL
+    .replace(/\n{3,}/g, '\n\n') // Collapse newlines down to at most 2
 }
 
 function element2md(node: Node): string {
-  let before = '';
-  let after = '';
-  const tagName = node.nodeName.toLowerCase();
-  switch (tagName) {
-    case '#text':
-      return node.textContent?.replace(/^\s+|\s+$/, ' ') ?? '';
-    case '#comment':
-      return `\n<!-- ${node.textContent ?? ''} -->\n`
-    case 'b':
-    case 'strong':
-      before = after = '**';
-      break;
-    case 'i':
-    case 'em':
-      before = after = '_';
-      break;
-    case 'code':
-      before = after = '`';
-      break;
-    case 'p':
-      before = after = '\n';
-      break;
-    case 'a':
-      if (node instanceof HTMLAnchorElement && node.href) {
-        before = '[';
-        after = '](' + node.href + ')';
-      }
-      break;
-    case 'img':
-      if (node instanceof HTMLImageElement && node.src) {
-        return `![${node.alt || node.title}](${node.src})`
-      }
-      break;
-    case 'h1':
-    case 'h2':
-    case 'h3':
-    case 'h4':
-    case 'h5':
-    case 'h6':
-      const level = (tagName.substr(1) as any) << 0;
-      const heading = new Array(level).fill('#').join('');
-      before = `\n${heading} `;
-      after = '\n';
-      break;
-    case 'pre':
-      before = after = '\n```\n';
-      break;
-    case 'span':
-      if (node instanceof HTMLSpanElement) {
-        const weight = node.style.fontWeight;
-        const style = node.style.fontStyle;
-        const bold = weight.includes('bold') || +weight > 400;
-        const italic = style.includes('italic');
-        if (bold && italic) {
-          before = '**_';
-          after = '_**';
-        } else if (bold) {
-          before = after = '**';
-        } else if (italic) {
-          before = after = '_';
-        }
-      }
-      break;
+  if (node instanceof HTMLImageElement && node.src) {
+    // image element - special case because no contents
+    // TODO put the title after the src, like [this](example "tada").
+    return `![${node.alt || node.title}](${node.src})`;
+  } else if (isBlock(node)) {
+    console.log("is block node: <" + node.tagName.toLowerCase() + ">");
+    // block element
+    if (isCode(node)) {
+      const fence = '\n```\n';
 
+      console.group("code block <" + node.nodeName + ">");
+      const contents = Array.from(node.childNodes)
+        .map(element2code)
+        .join('');
+      console.groupEnd();
+
+      const result = '\n' + fence + contents.replace(/^\n*?(\s*\n)?|(\n\s*)?\n*$/, '') + fence + '\n';
+      console.log(`code block <${node.nodeName}> result`, { result });
+      return result;
+    } else {
+
+      console.group(node.nodeName);
+      let contents = Array.from(node.childNodes)
+        .map(element2md)
+        .join('');
+      console.groupEnd();
+
+      const headingLevel = maybeHeadingLevel(node);
+      if (headingLevel && contents) {
+        const headingPrefix = new Array(headingLevel).fill('#').join('')
+        contents = headingPrefix + ' ' + contents.trim();
+      }
+
+      return contents.replace(/^\s*|\s*$/, '\n\n');
+    }
+  } else {
+    console.log("is inline node: <" + node.nodeName + ">");
+    // inline element
+    if (isComment(node)) {
+      const contents = node.textContent;
+      return contents ? ` <!-- ${contents} -->` : '';
+    }
+    if (isText(node)) {
+      return node.textContent?.replace(/^\s+|\s+$/, ' ') || '';
+    }
+    if (!(node instanceof HTMLElement)) {
+      return '';
+    }
+    let before = '';
+    let after = '';
+    if (node instanceof HTMLAnchorElement && node.href) {
+      // TODO put title after link [this](example "tada").
+      before = '[';
+      after = '](' + node.href.trim() + ')';
+    }
+    if (isBold(node)) {
+      console.log("bold!");
+      before = before + '**';
+      after = '**' + after;
+    }
+    if (isItalic(node)) {
+      console.log("italic!");
+      before = before + '_';
+      after = '_' + after;
+    }
+    if (isCode(node)) {
+      console.log("code!");
+      before = before + '`';
+      after = '`' + after;
+    }
+
+    console.group(node.nodeName);
+    let contents = Array.from(node.childNodes)
+      .map(element2md)
+      .join('');
+    console.groupEnd();
+
+    return contents ? before + contents + after : '';
   }
-  let contents = Array.from(node.childNodes)
-    .map(element2md)
+}
+
+function element2code(node: Node): string {
+  if (isText(node)) {
+    return node.textContent || '';
+  }
+  const contents = Array.from(node.childNodes)
+    .map(element2code)
     .join('');
-  const isBlock = node instanceof HTMLElement && isBlockLevel(node);
-  if (isBlock) {
-    contents = contents.trim();
+  if (isBlock(node)) {
+    return contents.replace(/^\n*|\n*$/, '\n');
+  } else {
+    return contents;
   }
-  contents = before + contents + after;
-  if (isBlock) {
-    contents = `\n\n${contents}\n\n`;
-  }
-  return contents;
 }
 
 const blockLevelTagNames = new Set([
@@ -88,6 +107,7 @@ const blockLevelTagNames = new Set([
   'article',
   'aside',
   'blockquote',
+  'body',
   'canvas',
   'dd',
   'div',
@@ -120,7 +140,10 @@ const blockLevelTagNames = new Set([
   'video',
 ]);
 
-function isBlockLevel(el: HTMLElement) {
+function isBlock(el: Node): el is HTMLElement {
+  if (!(el instanceof HTMLElement)) {
+    return false;
+  }
   const display = el.style.display;
   if (display.includes('inline')) {
     return false;
@@ -129,4 +152,37 @@ function isBlockLevel(el: HTMLElement) {
   } else {
     return blockLevelTagNames.has(el.tagName.toLowerCase());
   }
+}
+
+function isComment(el: Node): boolean {
+  return el.nodeName.toLowerCase() === '#comment';
+}
+
+function isText(el: Node): boolean {
+  return el.nodeName.toLowerCase() === '#text';
+}
+
+function isBold(el: HTMLElement): boolean {
+  if (el.tagName.toLowerCase() === 'b' || el.tagName.toLowerCase() === 'strong') {
+    return true;
+  }
+  const weight = el.style.fontWeight;
+  return weight.includes('bold') || +weight > 400;
+}
+
+function isItalic(el: HTMLElement): boolean {
+  return el.tagName.toLowerCase() === 'i'
+    || el.tagName.toLowerCase() === 'em'
+    || el.style.fontStyle.includes('italic');
+}
+
+function isCode(el: HTMLElement): boolean {
+  return el.tagName.toLowerCase() === 'code'
+    || el.tagName.toLowerCase() == 'pre'
+    || !!el.style.fontFamily.match(/\bmono(?:space)?\b/i);
+}
+
+function maybeHeadingLevel(el: HTMLElement): number | undefined {
+  const level = el.tagName.toLowerCase().match(/^h(\d)$/i)?.[1];
+  return level ? +level : undefined;
 }
