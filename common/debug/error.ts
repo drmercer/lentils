2cafe6d27278d15c5ba50cc94edc5f2f49e3a71f
Dan Mercer <danielmercer1@gmail.com>
2020-09-18 20:54:12 -0600

    Clean call stack to avoid overwhelming trello

--- common/debug/error.ts
+++ common/debug/error.ts
@@ -1,7 +1,14 @@
 export function error2Markdown(err: Error): string {
   const message = err.message?.trim();
   return (message ? labeledCodeBlock('Error message', message) : '') +
-    (err.stack ? labeledCodeBlock('Call stack', err.stack) : '');
+    (err.stack ? labeledCodeBlock('Call stack', cleanCallStack(err.stack)) : '');
+}
+
+export function cleanCallStack(stack: string): string {
+  return stack
+    .split('\n')
+    .slice(0, 10)
+    .join('\n');
 }
 
 export function labeledCodeBlock(label: string, blockText: string) {
