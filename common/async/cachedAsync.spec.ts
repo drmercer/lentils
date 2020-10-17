399b1809df56a33ae8be36a7ec905c8930584748
Dan Mercer <danielmercer1@gmail.com>
2020-10-17 14:57:57 -0600

    Add memoize() utility with unit tests

---
+++ common/async/cachedAsync.spec.ts
@@ -0,0 +1,97 @@
+import { memoize } from './cachedAsync';
+
+describe('memoize', () => {
+  it('should cache results correctly', async () => {
+    type Fn = (a: string, b: number) => Promise<string>;
+    const mockFn = jest.fn(async (a: string, b: number) => a + b);
+    const memoizedFn: Fn = memoize({}, mockFn);
+
+    expect(await memoizedFn('potato', 2)).toBe('potato2');
+    expect(await memoizedFn('potato', 2)).toBe('potato2');
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should cache results correctly with a custom key function', async () => {
+    type Fn = (a: string, b: number) => Promise<string>;
+    const mockFn = jest.fn(async (a: string, b: number) => a + b);
+    const memoizedFn: Fn = memoize({
+      keyFn: (a, _b) => a,
+    }, mockFn);
+
+    expect(await memoizedFn('potato', 2)).toBe('potato2');
+    // This assertion seems wrong, but it's because the keyFn only uses the first value
+    expect(await memoizedFn('potato', 1234567)).toBe('potato2');
+    expect(mockFn).toHaveBeenCalledTimes(1);
+
+    expect(await memoizedFn('bagel', 2)).toBe('bagel2');
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('should impose a default max cache size of 5', async () => {
+    type Fn = (a: string, b: number) => Promise<string>;
+    const mockFn = jest.fn(async (a: string, b: number) => a + b);
+    const memoizedFn: Fn = memoize({}, mockFn);
+
+    const defaultMaxCacheSize = 5;
+    for (let i = 0; i < defaultMaxCacheSize; i++) {
+      expect(await memoizedFn('foo', i)).toBe('foo' + i);
+    }
+
+    expect(mockFn).toHaveBeenCalledTimes(defaultMaxCacheSize);
+
+    // make all the calls again
+    for (let i = 0; i < defaultMaxCacheSize; i++) {
+      expect(await memoizedFn('foo', i)).toBe('foo' + i);
+    }
+    // make the 1st call again
+    expect(await memoizedFn('foo', 0)).toBe('foo' + 0);
+
+    expect(mockFn).toHaveBeenCalledTimes(defaultMaxCacheSize);
+
+    // make a 6th unique call, pushing the first one off the cache
+    expect(await memoizedFn('bagel', 1)).toBe('bagel1');
+
+    expect(mockFn).toHaveBeenCalledTimes(defaultMaxCacheSize + 1);
+
+    // make the 1st call again - should not hit the cache
+    expect(await memoizedFn('foo', 0)).toBe('foo' + 0);
+
+    expect(mockFn).toHaveBeenCalledTimes(defaultMaxCacheSize + 2);
+  });
+
+  it('should accept a maxCount option to customize the max cache size', async () => {
+    type Fn = (a: string, b: number) => Promise<string>;
+    const mockFn = jest.fn(async (a: string, b: number) => a + b);
+
+    const maxCacheSize = 5;
+
+    const memoizedFn: Fn = memoize({
+      maxCount: maxCacheSize,
+    }, mockFn);
+
+    for (let i = 0; i < maxCacheSize; i++) {
+      expect(await memoizedFn('foo', i)).toBe('foo' + i);
+    }
+
+    expect(mockFn).toHaveBeenCalledTimes(maxCacheSize);
+
+    // make all the calls again
+    for (let i = 0; i < maxCacheSize; i++) {
+      expect(await memoizedFn('foo', i)).toBe('foo' + i);
+    }
+    // make the 1st call again
+    expect(await memoizedFn('foo', 0)).toBe('foo' + 0);
+
+    expect(mockFn).toHaveBeenCalledTimes(maxCacheSize);
+
+    // make a 6th unique call, pushing the first one off the cache
+    expect(await memoizedFn('bagel', 1)).toBe('bagel1');
+
+    expect(mockFn).toHaveBeenCalledTimes(maxCacheSize + 1);
+
+    // make the 1st call again
+    expect(await memoizedFn('foo', 0)).toBe('foo' + 0);
+
+    expect(mockFn).toHaveBeenCalledTimes(maxCacheSize + 2);
+  });
+});
--- common/async/cachedAsync.ts
+++ common/async/cachedAsync.ts
@@ -1,6 +1,6 @@
 export function cachedAsync<T>(f: () => Promise<T>): (force?: boolean) => Promise<T> {
   let promise: Promise<T> | undefined = undefined;
-  return async (force?: boolean) => {
+  return (force?: boolean) => {
     if (!promise || force) {
       promise = f().catch((err) => {
         // clear cache if an error occurs, so the async thing can be redone
@@ -8,6 +8,60 @@ export function cachedAsync<T>(f: () => Promise<T>): (force?: boolean) => Promis
         throw err;
       });
     }
-    return await promise;
+    return promise;
+  };
+}
+
+interface MemoizeOptions<A extends unknown[], T> {
+  /** defaults to JSON.stringify(args) */
+  keyFn?: (...args: A) => string;
+  /** defaults to 5 */
+  maxCount?: number
+}
+
+const defaultKeyFn = ((...x: unknown[]) => JSON.stringify(x));
+const defaultMaxCount = 5;
+
+export function memoize<A extends unknown[], T>(
+  options: MemoizeOptions<A, T>,
+  f: (...args: A) => Promise<T>,
+): (...args: A) => Promise<T> {
+  const {
+    keyFn = defaultKeyFn,
+    maxCount = defaultMaxCount,
+  } = options;
+
+  let cache = new Map<string, Promise<T>>();
+  let cacheKeys: string[] = [];
+
+  return (...params: A) => {
+    const key = keyFn(...params);
+    let promise = cache.get(key);
+
+    if (!promise) {
+      promise = Promise.resolve().then(() => {
+        // this is wrapped in a .then() call just in case f throws an error
+        return f(...params);
+      }).then(result => {
+        cache.set(key, Promise.resolve(result));
+        return result;
+      }, err => {
+        cache.delete(key);
+        throw err;
+      });
+
+      cacheKeys.push(key);
+      cache.set(key, promise);
+
+      // Prune cache to a size of maxCount
+      if (cacheKeys.length > maxCount) {
+        const deleted = cacheKeys.splice(0, cacheKeys.length - maxCount);
+        for (const keyToDelete of deleted) {
+          cache.delete(keyToDelete);
+        }
+      }
+    }
+
+    return promise;
   };
 }
