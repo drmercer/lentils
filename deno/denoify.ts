3639a313d8649d485cb3a5adf1005f40ffc27345
Dan Mercer <danielmercer1@gmail.com>
2020-11-12 22:46:24 -0700

    Improve denoify to support multiple mappings

--- deno/denoify.ts
+++ deno/denoify.ts
@@ -5,32 +5,48 @@ async function hasPermission(name: "read"|"write", path: string): Promise<boolea
   return (await Deno.permissions.request({name, path})).state === "granted";
 }
 
-const mapPath = "./denoify-map.json";
+if (import.meta.main) {
+  const mapPath = "./denoify-map.json";
 
-if (!hasPermission("read", dirname(mapPath))) {
-  console.error(`Error: permission is needed to read the denoify map file '${mapPath}'`)
+  if (!hasPermission("read", dirname(mapPath))) {
+    console.error(`Error: permission is needed to read the denoify map file '${mapPath}'`)
+  }
+
+  const map = JSON.parse(await Deno.readTextFile(mapPath));
+  Deno.chdir(dirname(mapPath));
+
+  for (const fromDir of Object.keys(map)) {
+    await transformDir(fromDir, map[fromDir]);
+  }
 }
 
-const map = JSON.parse(await Deno.readTextFile(mapPath));
-Deno.chdir(dirname(mapPath));
+export async function transformDir(fromDir: string, toDir: string) {
 
-// TODO actually support multiple mappings
-const fromDir = Object.keys(map)[0];
-const toDir = map[fromDir];
+  if (!hasPermission("read", fromDir)) {
+    console.error(`Error: permission is needed to read the fromDir '${fromDir}'`);
+    Deno.exit(1);
+  }
+  if (!hasPermission("write", toDir)) {
+    console.error(`Error: permission is needed to write the toDir '${toDir}'`);
+    Deno.exit(1);
+  }
 
-if (!hasPermission("read", fromDir)) {
-  console.error(`Error: permission is needed to read the fromDir '${fromDir}'`);
-  Deno.exit(1);
+  await Deno.mkdir(toDir, {recursive: true});
+  await Deno.remove(toDir, {recursive: true});
+  await Deno.mkdir(toDir, {recursive: true});
+  Deno.chdir(fromDir);
+
+  for await (const entry of walk(fromDir)) {
+    if (entry.isFile && shouldTransform(entry.path)) {
+      const fromPath = entry.path;
+      const toPath = toDir + relative(fromDir, fromPath);
+      const toSubdir = dirname(toPath);
+
+      await Deno.mkdir(toSubdir, {recursive: true});
+      await transformFile(fromPath, toPath);
+    }
+  }
 }
-if (!hasPermission("write", toDir)) {
-  console.error(`Error: permission is needed to write the toDir '${toDir}'`);
-  Deno.exit(1);
-}
-
-await Deno.mkdir(toDir, {recursive: true});
-await Deno.remove(toDir, {recursive: true});
-await Deno.mkdir(toDir, {recursive: true});
-Deno.chdir(fromDir);
 
 function shouldTransform(path: string) {
   return path.endsWith('.ts') &&
@@ -38,22 +54,11 @@ function shouldTransform(path: string) {
     !path.endsWith('.spec.ts');
 }
 
-async function transform(fromPath: string, toPath: string) {
+async function transformFile(fromPath: string, toPath: string) {
   console.log(`Transforming ${fromPath} to ${toPath}`);
   const original = await Deno.readTextFile(fromPath);
   const transformed = original.replace(/^(import [\s\S]*?)(['"];)$/gm, '$1.ts$2');
   await Deno.writeTextFile(toPath, transformed);
-}
-
-for await (const entry of walk(fromDir)) {
-  if (entry.isFile && shouldTransform(entry.path)) {
-    const fromPath = entry.path;
-    const toPath = toDir + relative(fromDir, fromPath);
-    const toSubdir = dirname(toPath);
-
-    await Deno.mkdir(toSubdir, {recursive: true});
-    await transform(fromPath, toPath);
-  }
 }
 
 // TODO support watch mode
