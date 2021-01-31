0296f912dc046cf6d99f5b7c15f3bb322736830d
Dan Mercer <danielmercer1@gmail.com>
2021-01-30 22:47:02 -0700

    Add rangeOverlap utility

--- common/math/math.spec.ts
+++ common/math/math.spec.ts
@@ -1,4 +1,4 @@
-import { clamp } from './math';
+import { clamp, rangeOverlap } from './math';
 
 describe('clamp', () => {
 
@@ -6,6 +6,26 @@ describe('clamp', () => {
     expect(clamp(2, 1, 3)).toBe(2);
     expect(clamp(10, 1, 3)).toBe(3);
     expect(clamp(0, 1, 3)).toBe(1);
+  });
+
+});
+
+describe('rangeOverlap', () => {
+
+  it('should handle forward overlap', () => {
+    expect(rangeOverlap(1, 3, 2, 4)).toEqual([2, 3]);
+  });
+
+  it('should handle backward overlap', () => {
+    expect(rangeOverlap(2, 4, 1, 3)).toEqual([2, 3]);
+  });
+
+  it('should handle inner overlap', () => {
+    expect(rangeOverlap(2, 3, 1, 4)).toEqual([2, 3]);
+  });
+
+  it('should handle outer overlap', () => {
+    expect(rangeOverlap(1, 4, 2, 3)).toEqual([2, 3]);
   });
 
 });
--- common/math/math.ts
+++ common/math/math.ts
@@ -1,3 +1,23 @@
 export function clamp(x: number, min: number, max: number): number {
   return Math.max(min, Math.min(x, max));
 }
+
+function isBetween(x: number, a: number, b: number) {
+  return (x >= a && x < b);
+}
+
+export function rangeOverlap(a: number, b: number, x: number, y: number): [number, number]|undefined {
+  if (isBetween(a, x, y)) {
+    if (isBetween(b, x, y)) {
+      return [a, b];
+    } else {
+      return [a, y];
+    }
+  } else if (isBetween(b, x, y)) {
+    return [x, b];
+  } else if (isBetween(x, a, b)) {
+    return [x, y];
+  } else {
+    return undefined;
+  }
+}
