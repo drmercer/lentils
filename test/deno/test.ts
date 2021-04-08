8002823a3e12183ce9fd4582d907b6b332422278
Dan Mercer <danielmercer1@gmail.com>
2021-04-07 21:11:56 -0600

    Add new v2 injector

--- test/deno/test.ts
+++ test/deno/test.ts
@@ -1,6 +1,7 @@
 import { isString } from '../../common/types/checks.ts';
 import { Emitter } from '../../common/events/emitter.ts';
 import { standardTemplate, demargin } from '../../deno/denoified-common/string/string.ts';
+import { Injector, injectable, InjectorKey } from '../../injector/v2/injector.ts';
 import { assertEquals } from "https://deno.land/std@0.77.0/testing/asserts.ts";
 
 Deno.test("isString is function", () => {
@@ -26,4 +27,43 @@ Deno.test("demargin works", () => {
     bar ${2}
   `;
   assertEquals(s, "foo 1\nbar 2\n");
+});
+
+Deno.test("Injector v2 works", () => {
+
+  const A = injectable('A', [], () => {
+    return {
+      foo: 'a',
+    };
+  });
+
+  const B = injectable('B', [A] as const, (a) => {
+    function getA(): unknown {
+      return a;
+    }
+
+    return {
+      getA,
+      bar: 'b' + a.foo,
+    };
+  });
+
+  const C = injectable('C', [A, B, InjectorKey] as const, (a, b, injector) => {
+    return {
+      bagel: 'c' + a.foo + b.bar,
+      injector,
+    };
+  });
+
+  const injector = new Injector();
+  const c = injector.get(C);
+  const b = injector.get(B)
+  const a = injector.get(A);
+
+  assertEquals(b.bar, 'ba');
+  assertEquals(c.bagel, 'caba');
+
+  assertEquals(b.getA(), a);
+  assertEquals(c.injector, injector);
+
 });
