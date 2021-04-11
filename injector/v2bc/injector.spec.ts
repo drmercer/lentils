5e7a0f0c073768697f93d10ab1eb0fd3fca93913
Dan Mercer <danielmercer1@gmail.com>
2021-04-11 00:17:49 -0600

    Add backwards-compat version of v2 injector

---
+++ injector/v2bc/injector.spec.ts
@@ -0,0 +1,178 @@
+import { Injectable, injectable, InjectKey, Injector, override, UseInjectKey } from './injector';
+
+interface A {
+  foo: string;
+}
+
+// The following 3 injectables all provide an implementation of the "A" interface
+
+const A: InjectKey<A> = injectable('A', () => {
+  return {
+    foo: 'a',
+  };
+});
+
+const A2: InjectKey<A> = injectable('A2', () => {
+  return {
+    foo: 'a2',
+  };
+});
+
+const A3: InjectKey<A> = injectable('A3', () => {
+  return {
+    foo: 'a3',
+  };
+});
+
+// Using a named interface for an injectable is nice, but not necessary - here's an example where TS
+// just infers the InjectKey's type from the factory function's return value:
+const B = injectable('B', A, (a) => {
+  function getA(): unknown {
+    return a;
+  }
+
+  return {
+    getA,
+    bar: 'b' + a.foo,
+  };
+});
+
+// Compat stuff
+
+@Injectable()
+class ClassBased1 {
+  foo = "hello";
+}
+
+@Injectable()
+class ClassBased2 {
+  constructor(
+    public cb1: ClassBased1,
+    @UseInjectKey(A) public a: A,
+  ) {
+
+  }
+}
+
+// This demonstrates how to express an optional dependency - just use a key that defaults to undefined, and
+// then override it in the injector if needed
+const OptionalA: InjectKey<A | undefined> = injectable('OptionalA', () => undefined);
+
+// This demonstrates another advanced usage - injecting the injector. This allows C to do injection at
+// runtime by calling injector.get().
+const C = injectable('C', A, B, Injector.Self, ClassBased2, OptionalA, (a: A, b, injector, cb2, maybeA?: A) => {
+  return {
+    bagel: 'c' + a.foo + b.bar,
+    injector,
+    hasOptionalA: maybeA !== undefined,
+    cb2a: cb2.a,
+    cb2,
+  };
+});
+
+// Type tests:
+
+// Should only be able to override a key with a key/value assignable to it
+new Injector([
+  // @ts-expect-error Should reject because doesn't include `foo: string`
+  override(A).withValue({ foo2: 'A' }),
+]);
+
+// @ts-expect-error Should properly type the InjectKey based on the return value of the factory fn
+const BadReturnValue: InjectKey<{ a: boolean }> = injectable(
+  'BadReturnValue',
+  A,
+  B,
+  Injector.Self,
+  (a, b, injector) => {
+    return {
+      b: true,
+    };
+  });
+
+// @ts-expect-error Should complain if extra deps are expected
+const UndeclaredDep = injectable('UndeclaredDep', A, (a, b) => {
+  return {
+    foo: true,
+  };
+});
+
+// @ts-expect-error Should complain if deps are typed incorrectly
+const MisdeclaredDep = injectable('MisdeclaredDep', A, (a: { bar: string }) => {
+  return {
+    foo: true,
+  };
+});
+
+// Behavior tests:
+
+describe('injector v2', () => {
+  it('should work', () => {
+    const injector = new Injector();
+    const c = injector.get(C);
+    const b = injector.get(B)
+    const a = injector.get(A);
+    const cb2 = injector.get(ClassBased2);
+
+    expect(b.bar).toEqual('ba');
+    expect(c.bagel).toEqual('caba');
+
+    expect(b.getA()).toBe(a);
+    expect(c.injector).toBe(injector);
+    expect(c.hasOptionalA).toBe(false);
+    expect(c.cb2a).toBe(a);
+    expect(c.cb2).toBe(cb2);
+  })
+
+  it('should allow overriding with key', () => {
+    const injector = new Injector([
+      override(A).withOther(A2),
+    ]);
+    const c = injector.get(C);
+    const b = injector.get(B)
+    const a = injector.get(A);
+
+    expect(a.foo).toEqual('a2');
+    expect(b.bar).toEqual('ba2');
+    expect(c.bagel).toEqual('ca2ba2');
+
+    expect(b.getA()).toBe(a);
+    expect(c.injector).toBe(injector);
+  })
+
+  it('should allow overriding with key (optional dep, for demonstration)', () => {
+    const injector = new Injector([
+      override(OptionalA).withOther(A),
+    ]);
+    const c = injector.get(C);
+
+    expect(c.hasOptionalA).toEqual(true);
+  })
+
+  it('should allow overriding with value', () => {
+    const injector = new Injector([
+      override(A).withValue({ foo: 'A' }),
+    ]);
+    const c = injector.get(C);
+    const b = injector.get(B)
+    const a = injector.get(A);
+
+    expect(a.foo).toEqual('A');
+    expect(b.bar).toEqual('bA');
+    expect(c.bagel).toEqual('cAbA');
+
+    expect(b.getA()).toBe(a);
+    expect(c.injector).toBe(injector);
+  })
+
+  it('should throw if override loop exists', () => {
+    const injector = new Injector([
+      override(A).withOther(A2),
+      override(A2).withOther(A3),
+      override(A3).withOther(A),
+    ]);
+    expect(() => {
+      injector.get(A);
+    }).toThrowError(/^Circular override dependencies: A -> A2 -> A3 -> A$/);
+  })
+})
---
+++ injector/v2bc/injector.ts
@@ -0,0 +1,174 @@
+import { isArray, isFunction, isUndefined, isString } from '../../common/types/checks';
+import { Constructor } from '../../common/types/meta';
+import 'reflect-metadata';
+
+// V1 STUFF
+
+const isInjectableKey = 'dm:injectable';
+
+/**
+ * @deprecated replace with injectable() instead
+ */
+export function Injectable() {
+  return function (ctor: Constructor<unknown>) {
+    Reflect.defineMetadata(isInjectableKey, true, ctor);
+  }
+}
+
+export function isInjectable(ctor: Constructor<unknown>): boolean {
+  return !!Reflect.getMetadata(isInjectableKey, ctor);
+}
+
+
+// COMPAT STUFF
+
+export type AbstractInjectKey<T> = InjectKey<T> | Constructor<T>;
+
+const injectKeyForParam = Symbol("injectKeyForParam");
+
+export function UseInjectKey<T>(key: InjectKey<T>) {
+  return function (target: Constructor<unknown>, propertyKey: string, parameterIndex: number): void {
+    if (propertyKey !== undefined) {
+      throw new Error("UseInjectKey must only be used on the constructor!");
+    }
+    const keysByParameter: Map<number, InjectKey<unknown>> = getParamInjectKeys(target);
+    keysByParameter.set(parameterIndex, key);
+    Reflect.defineMetadata(injectKeyForParam, keysByParameter, target);
+  }
+}
+
+function getParamInjectKeys(target: Constructor<unknown>): Map<number, InjectKey<unknown>> {
+  return Reflect.getOwnMetadata(injectKeyForParam, target) || new Map();
+}
+
+// V2 STUFF
+
+class InjectKey<T> {
+  private IfYoureSeeingThisInAnErrorMessageItMeansYoureTryingToUseSomethingAsAnInjectKeyWhenItsNotOne!: T;
+  constructor(
+    public injectableName: string,
+  ) { }
+}
+export type { InjectKey };
+
+export type InjectedValue<K extends AbstractInjectKey<unknown>> = K extends AbstractInjectKey<infer T> ? T : never;
+
+type DepValues<DepKeys extends readonly AbstractInjectKey<unknown>[]> = {
+  [K in keyof DepKeys]: DepKeys[K] extends AbstractInjectKey<unknown> ? InjectedValue<DepKeys[K]> : never;
+}
+
+interface InjectableData<T> {
+  deps: readonly AbstractInjectKey<unknown>[];
+  factory: (...args: any) => T;
+}
+
+const metadata = new WeakMap<InjectKey<unknown>, InjectableData<unknown>>();
+
+export function injectable<T, DepKeys extends readonly AbstractInjectKey<unknown>[]>(
+  name: string,
+  // Makes me sad that this signature isn't very clean, but at least it's user-friendly and doesn't require "as const"
+  ...rest: [
+    ...deps: DepKeys,
+    factory: (...args: DepValues<DepKeys>) => T,
+  ]
+): InjectKey<T> {
+  const deps = rest.slice(0, -1);
+  const [factory] = rest.slice(-1);
+  const key = new InjectKey<T>(name);
+  metadata.set(key, {
+    deps: deps as unknown as DepKeys,
+    factory: factory as (...args: DepValues<DepKeys>) => T,
+  });
+  return key;
+}
+
+export interface Override<T> {
+  overridden: InjectKey<T>;
+  overrider: InjectKey<T>;
+}
+
+export function override<T>(overridden: InjectKey<T>) {
+  return {
+    withOther(overrider: InjectKey<T>): Override<T> {
+      return { overridden, overrider };
+    },
+    withValue(value: T): Override<T> {
+      return {
+        overridden,
+        overrider: injectable<T, []>('explicit value', () => value),
+      };
+    },
+  };
+}
+
+export class Injector {
+  public static Self: InjectKey<Injector> = new InjectKey('Injector');
+
+  private instances: WeakMap<AbstractInjectKey<unknown>, any> = new WeakMap();
+  private overrides: Map<InjectKey<unknown>, InjectKey<unknown>>;
+
+  constructor(overrides: Override<unknown>[] = []) {
+    this.overrides = new Map(overrides.map(o => [o.overridden, o.overrider]));
+  }
+
+  public get<T>(key: AbstractInjectKey<T>): T {
+    return this._get(key, []);
+  }
+
+  private _get<T>(key: AbstractInjectKey<T>, overriddenKeys: InjectKey<T>[]): T {
+    if (!isFunction(key) && this.overrides.has(key)) {
+      // Compat ^ (only allow overriding InjectKeys)
+      const overrider = this.overrides.get(key) as InjectKey<T>;
+      const newOverriddenKeys = [...overriddenKeys, key];
+      // Check for circular dependency
+      if (newOverriddenKeys.includes(overrider)) {
+        const message: string = [...newOverriddenKeys, overrider].map(k => k.injectableName).join(' -> ');
+        throw new Error("Circular override dependencies: " + message);
+      }
+      return this._get(overrider, newOverriddenKeys);
+    }
+    if (this.instances.has(key)) {
+      return this.instances.get(key);
+    }
+    if (key === Injector.Self as AbstractInjectKey<unknown>) {
+      return this as unknown as T;
+    }
+    const instance = this.create(key);
+    this.instances.set(key, instance);
+    return instance;
+  }
+
+  private create<T>(key: AbstractInjectKey<T>): T {
+    if (isFunction(key)) {
+      // Compat
+      const paramTypes = this.getCtorParams(key);
+      const params = paramTypes.map(c => this.get(c));
+      if (params.length !== key.length) {
+        console.warn(`Expected ${key.length} parameters for ${key.name} constructor, but only found ${params.length}!`);
+      }
+      return new key(...params);
+    } else {
+      const { factory, deps } = metadata.get(key) as InjectableData<T>;
+      const depValues = deps.map(dep => this.get(dep));
+      return factory(...depValues);
+    }
+  }
+
+  // Compat
+  private getCtorParams<T>(ctor: Constructor<T>): AbstractInjectKey<unknown>[] {
+    if (!isInjectable(ctor)) {
+      throw new Error(`${ctor.name} is not Injectable!`);
+    }
+    const metadata: unknown = Reflect.getMetadata('design:paramtypes', ctor);
+    if (isArray(metadata) && metadata.every(isFunction)) {
+      const ctors = metadata as Constructor<unknown>[];
+      const paramKeys = getParamInjectKeys(ctor);
+      return ctors.map((c, idx) => paramKeys.get(idx) || c);
+    } else {
+      if (!isUndefined(metadata)) {
+        console.warn(`Found weird metadata for Constructor '${ctor.name}':`, metadata);
+      }
+      return [];
+    }
+  }
+}
