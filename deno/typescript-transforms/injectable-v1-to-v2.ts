ee32d6d20ac8b4f92cf35c5781e03d21d356da36
Dan Mercer <danielmercer1@gmail.com>
2021-04-24 00:25:24 -0600

    Code cleanup in injectable-v1-to-v2

--- deno/typescript-transforms/injectable-v1-to-v2.ts
+++ deno/typescript-transforms/injectable-v1-to-v2.ts
@@ -75,24 +75,8 @@ export const ${className} = injectable('${className}', (inject) => {
 }
 
 function injectableClassMembersToStatements(members: readonly TS.ClassElement[]): string {
-  const exported: string[] = [];
   const ctor = members.find(m => ts.isConstructorDeclaration(m)) as TS.ConstructorDeclaration;
-  let injections: string[];
-  if (ctor) {
-    const params = ctor.parameters.map(p => {
-      const name: string = ts.isIdentifier(p.name) ? p.name.escapedText.toString() : p.name.toString();
-      const typeName: string = p.type && ts.isTypeReferenceNode(p.type) && ts.isIdentifier(p.type.typeName) && p.type.typeName.escapedText.toString() || "null as any";
-      const isPublic = p.modifiers?.some(m => m.kind === ts.SyntaxKind.PublicKeyword) ?? false;
-      return { name, typeName, isPublic };
-    });
-    injections = params.map(({ name, typeName }) => {
-      return `const ${name} = inject(${typeName});`;
-    });
-    const exportedParams = params.filter(({ isPublic }) => isPublic).map(({ name }) => name);
-    exported.push(...exportedParams);
-  } else {
-    injections = [];
-  }
+  const {exported, injections} = ctor ? ctorParamsToInjections(ctor.parameters) : {exported: [], injections: []};
   const transformedMembers: string[] = members
     .map<string>(m => {
       if (ts.isConstructorDeclaration(m)) {
@@ -133,6 +117,23 @@ ${comment}${async ? 'async ' : ''}function ${name}${typeParams}(${params}) {
     ...transformedMembers,
     returnedObject(exported),
   ].join('\n\n').trim();
+}
+
+function ctorParamsToInjections(parameters: TS.NodeArray<TS.ParameterDeclaration>) {
+  const params = parameters.map(p => {
+    const name: string = ts.isIdentifier(p.name) ? p.name.escapedText.toString() : p.name.toString();
+    const typeName: string = p.type && ts.isTypeReferenceNode(p.type) && ts.isIdentifier(p.type.typeName) && p.type.typeName.escapedText.toString() || "null as any";
+    const isPublic = p.modifiers?.some(m => m.kind === ts.SyntaxKind.PublicKeyword) ?? false;
+    return { name, typeName, isPublic };
+  });
+  const injections = params.map(({ name, typeName }) => {
+    return `const ${name} = inject(${typeName});`;
+  });
+  const exported = params.filter(({ isPublic }) => isPublic).map(({ name }) => name);
+  return {
+    injections,
+    exported,
+  };
 }
 
 function returnedObject(names: string[]): string {
