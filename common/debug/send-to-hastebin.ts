5f876483d9bb2f17ec01d2b693ae937958a00d6a
Dan Mercer <danielmercer1@gmail.com>
2022-01-08 12:06:07 -0700

    Delete send-to-hastebin utility
    Hastebin doesn't support CORS so this isn't very useful. Also Hastebin
    was acquired, so hastes have a shorter lifespan now (I think).

--- common/debug/send-to-hastebin.ts
+++
@@ -1,8 +0,0 @@
-export async function hastebin(text: string, ext = 'md') {
-  const res = await fetch("https://hastebin.com/documents", {
-    method: "POST",
-    body: text,
-  });
-  const json = await res.json();
-  return `https://hastebin.com/${json.key}.${ext}`;
-}
